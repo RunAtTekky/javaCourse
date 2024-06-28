bb30c54e2985c7574c2a4bb6f5671d29beefd848
Varun <varunrawat343@gmail.com>
2024-06-28 15:31:04 +0530

    Add 2 questions

---
+++ ArmstrongAndPerfect.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArmstrongAndPerfect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        // Armstrong Number -> sum of cube of digits == number
+        // Perfect Number -> sum of proper divisors == number
+
+        boolean isArmstrong = isArmstrong(x);
+
+        if (isArmstrong) {
+            System.out.println(x + " is armstrong");
+        } else {
+            System.out.println(x + " is NOT armstrong");
+        }
+
+        boolean isPerfect = isPerfect(x);
+
+        if (isPerfect) {
+            System.out.println(x + " is perfect");
+        } else {
+            System.out.println(x + " is not perfect");
+        }
+    }
+
+    static boolean isArmstrong(int x) {
+        int sum = 0;
+        int ogX = x;
+        while (x > 0) {
+            int digit = x%10;
+            sum += digit*digit*digit;
+
+            x /= 10;
+        }
+        return sum==ogX;
+    }
+
+    static boolean isPerfect(int x) {
+        ArrayList<Integer> divisors = getDivisors(x);
+
+        int sum = 0;
+        for (int i=0; i<divisors.size(); i++) {
+            sum += divisors.get(i);
+        }
+        return sum==x;
+    }
+
+    static ArrayList<Integer> getDivisors(int x) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+
+        for (int i=1; i<x; i++) {
+            if (x%i == 0) {
+                divisors.add(i);
+            }
+        }
+
+        return divisors;
+    }
+}
---
+++ PrimeOrNot.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        boolean prime = true;
+        for (int i=2; i<x; i++) {
+            if (x%i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime) {
+            System.out.println(x + " is prime");
+        } else {
+            System.out.println(x + " is NOT prime");
+        }
+    }
+}
